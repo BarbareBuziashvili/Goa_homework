d86a38a6e5a389d029b4f4b9164556822103783b
qweqwe1qq <githubsaba@gmail.com>
2024-07-06 20:11:33 +0400

    first commit

---
+++ level 06/homework/level6.py
@@ -0,0 +1,129 @@
+name="barbare"
+last_name="buziashvili"
+age="10"
+hobby="programming"
+country="Georgia"
+city="rustavi"
+faorite_color="red"
+faorite_food="shawrma"
+print(name,last_name,age,hobby,country,city,faorite_color,faorite_food)
+
+
+a = 5
+b = 10
+c = 15
+d = 20
+e = 25
+f = 30
+g = 35
+h = 40
+i = 45
+j = 50
+
+
+result1 = a + b
+result2 = c - d
+result3 = e * f
+result4 = g / h
+result5 = a - 2
+result6 = b // 3
+result7 = c + e
+result8 = d * 2
+result9 = f / 4
+result10 = g + a
+
+result11 = h - b
+result12 = i * c
+result13 = j / d
+result14 = b + 3
+result15 = c // 5
+result16 = d + f
+result17 = e * 3
+result18 = f / 2
+result19 = g - a
+result20 = h * b
+
+result21 = i / c
+result22 = a + g
+result23 = b - h
+result24 = c * i
+result25 = d / j
+result26 = b / 2
+result27 = c // 4
+result28 = d + e
+result29 = f * 2
+result30 = g / 5
+
+result31 = h + c
+result32 = i - d
+result33 = j * a
+result34 = b / 4
+result35 = c * 3
+result36 = d // 2
+result37 = e + f
+result38 = f * g
+result39 = g / 3
+result40 = h - i
+
+result41 = j / 2
+result42 = a + d
+result43 = b * e
+result44 = c / f
+result45 = d ** 2
+result46 = e // 4
+result47 = f + g
+result48 = g - h
+result49 = h * i
+result50 = i / j
+
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
+print(result7)
+print(result8)
+print(result9)
+print(result10)
+print(result11)
+print(result12)
+print(result13)
+print(result14)
+print(result15)
+print(result16)
+print(result17)
+print(result18)
+print(result19)
+print(result20)
+print(result21)
+print(result22)
+print(result23)
+print(result24)
+print(result25)
+print(result26)
+print(result27)
+print(result28)
+print(result29)
+print(result30)
+print(result31)
+print(result32)
+print(result33)
+print(result34)
+print(result35)
+print(result36)
+print(result37)
+print(result38)
+print(result39)
+print(result40)
+print(result41)
+print(result42)
+print(result43)
+print(result44)
+print(result45)
+print(result46)
+print(result47)
+print(result48)
+print(result49)
+print(result50)
---
+++ level 08/homework.py
@@ -0,0 +1,21 @@
+lemur=10
+lemur_mango=5
+print(lemur-lemur_mango)
+
+# 2 print("Hello, world!") 
+
+# 3  x = 5
+#    y = 4
+#    print(x + y)
+
+# 4  a = 5
+#    b = 2
+#    print(a / b)
+
+# 5  x = 5
+#    y = 10
+#    z = 8
+#    print(z)
+
+# 6 age = 25
+#   print(age + 5)
